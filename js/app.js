303cc9c76221f6d170c8585feb082d9182ca17bc
JangKroed <96761280+JangKroed@users.noreply.github.com>
2022-08-31 21:32:59 +0900

    333

--- js/app.js
+++ js/app.js
@@ -5,13 +5,13 @@ let videoDay = [];
 let videoView = [];
 
 $(document).ready(() => {
-  listing();
+    listing();
 
-  setTimeout(() => {
-    const l = videoTitle.length;
+    setTimeout(() => {
+        const l = videoTitle.length;
 
-    for (i = 0; i < l; i++) {
-      const temp_html = `<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
+        for (i = 0; i < l; i++) {
+            const temp_html = `<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
                                             <figure class="effect-ming tm-video-item">
                                                 <figcaption class="d-flex align-items-center justify-content-center">
                                                     <a href="${videoUrl[i]}"><img src="${videoImage[i]}" alt="Image" class="img-fluid"></a>
@@ -23,47 +23,47 @@ $(document).ready(() => {
                                                 <span>${videoView[i]} views</span>
                                             </div>
                                         </div>`;
-      $("#videos").append(temp_html);
-      console.log(videoTitle.slice(10));
-      console.log(videoUrl.slice(10));
-      console.log(videoImage.slice(10));
-      console.log(videoDay.slice(10));
-      console.log(videoView.slice(10));
+            $("#videos").append(temp_html);
+            console.log(videoTitle.slice(10));
+            console.log(videoUrl.slice(10));
+            console.log(videoImage.slice(10));
+            console.log(videoDay.slice(10));
+            console.log(videoView.slice(10));
 
-      const temp_image = `<img class="main-image" src="${videoImage[0]}" />`;
-      $("#image-container").append(temp_image);
-    }
-  }, 10);
+            const temp_image = `<img class="main-image" src="${videoImage[0]}" />`;
+            $("#image-container").append(temp_image);
+        }
+    }, 10);
 });
 
 function listing() {
-  $.ajax({
-    type: "GET",
-    url: "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&regionCode=KR&videoCategoryId=20&key=AIzaSyCWWcxidOOzDR8SbEofjAUVdp2u51YtXj0",
-    async: false,
-    data: {},
-    success: (response) => {
-      const rows = response.items;
+    $.ajax({
+        type: "GET",
+        url: "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&regionCode=KR&videoCategoryId=20&key=AIzaSyCWWcxidOOzDR8SbEofjAUVdp2u51YtXj0",
+        async: false,
+        data: {},
+        success: (response) => {
+            const rows = response.items;
 
-      rows.map((video) => {
-        videoTitle.push(video.snippet.title);
-        videoUrl.push("https://youtu.be/" + video.id);
-        videoImage.push(video.snippet.thumbnails.high.url);
-        videoDay.push(video.snippet.publishedAt.substring(0, 10));
-      });
-    },
-  });
-  $.ajax({
-    type: "GET",
-    url: "https://www.googleapis.com/youtube/v3/videos?part=statistics&chart=mostPopular&maxResults=30&regionCode=KR&key=AIzaSyCWWcxidOOzDR8SbEofjAUVdp2u51YtXj0",
-    async: false,
-    data: {},
-    success: (response) => {
-      const rows = response.items;
+            rows.map((video) => {
+                videoTitle.push(video.snippet.title);
+                videoUrl.push("https://youtu.be/" + video.id);
+                videoImage.push(video.snippet.thumbnails.high.url);
+                videoDay.push(video.snippet.publishedAt.substring(0, 10));
+            });
+        },
+    });
+    $.ajax({
+        type: "GET",
+        url: "https://www.googleapis.com/youtube/v3/videos?part=statistics&chart=mostPopular&maxResults=30&regionCode=KR&key=AIzaSyCWWcxidOOzDR8SbEofjAUVdp2u51YtXj0",
+        async: false,
+        data: {},
+        success: (response) => {
+            const rows = response.items;
 
-      rows.map((video) => {
-        videoView.push(video.statistics.viewCount);
-      });
-    },
-  });
+            rows.map((video) => {
+                videoView.push(video.statistics.viewCount);
+            });
+        },
+    });
 }
